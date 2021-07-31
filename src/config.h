54cfee55d94f9a31396f5de1e2c8a6e90188e352
yonglak <67819854+yonglak@users.noreply.github.com>
2021-07-31 14:13:41 +0800

    Add files via upload
    Apple2 Sdisk with LCD
    	2010.11.11 by Koichi Nishida
    	2012.01.26 by Fábio Belavenuto
    Translated to English

---
+++ src/config.h
@@ -0,0 +1,22 @@
+/*
+ * config.h
+ *
+ *  Created on: 25/01/2012
+ *      Author: fabio
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/* by Fabio - Definições da porta D para comunicação com o SD */
+#define _CLK_DI_CS	0b11110010
+#define _CLKNDI_CS	0b11100010
+#define NCLK_DI_CS	0b11010010
+#define NCLKNDI_CS	0b11000010
+#define _CLK_DINCS	0b11110000
+#define _CLKNDINCS	0b11100000
+#define NCLK_DINCS	0b11010000
+#define NCLKNDINCS	0b11000000
+
+
+#endif /* CONFIG_H_ */
